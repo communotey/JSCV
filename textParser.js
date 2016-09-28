a378d69fedb991158bf78a119f50e0ac21bed091
Chris Cates <codeviolet@chriscates.ca>
2016-09-27 21:29:20 -0400

    Mapped the shapes of the font to each individual matrix

--- textParser.js
+++ textParser.js
@@ -1,10 +1,10 @@
 const pngToMatrix = require('png-to-matrix');
 
-module.exports = (image, color, variance) => {
+module.exports = (image, color, variance, callback) => {
 
 	pngToMatrix(image, (matrix) => {
 
-		//Indentiy all the valid points in the matrix
+		//Indentify all the valid points in the matrix
 		var points = matrix.map((y, i) => {
 			return y.map((x, ii) => {
 				if (
@@ -20,50 +20,75 @@ module.exports = (image, color, variance) => {
 			});
 		});
 
-		//For the test case, get the shape of lowercase A first
-		var shapeOfA = points.filter((y, i) => {
-			if (i < 35 && y.filter((point, ii) => { return point > 0; }).length > 0) {
-				return true;
-			} else {
-				return false;
-			}
-		}).map((y, i) => {
-			return y.map((point, ii) => {
-					return [point, ii];
-			}).filter((point, ii) => {
-				if (point[0] > 0) {
-					return true;
-				} else {
-					return false;
+		var alphabet = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+		var shapes = [];
+		var offset = 0;
+
+		for (i = 0; i < alphabet.length; i++) {
+			
+			var initialShape = [];
+			var hit = false;
+			var skipCount = 0;
+			var skip = false;
+			var ii = offset;
+
+			while (skip === false && ii < points.length) {
+				if (points[ii].filter((x) => { return x === 1 }).length > 0) {
+					var mappedPoints = points[ii].map((p, iii) => {
+						return [p, iii];
+					});
+					initialShape.push(mappedPoints);
+					hit = true;
+					skipCount = 0;
+				} 
+
+				if (hit === true && points[ii].filter((x) => { return x === 1 }).length === 0) {
+					skipCount++;
+					if (skipCount > 5) {
+						offset = ii + 1;
+						skip = true;
+					}
 				}
+				ii++;
+			}
+
+			//Map the matrix's indices to a hashtable to get the min and max range of X
+			var xMinRange, xMaxRange;
+			var indices = [];
+
+			initialShape.map((y, iii) => {
+				y.map((point, iiii) => {
+					indices.push(point[1]);
+				});
 			});
+
+			xMinRange = Math.min.apply(Math, indices);
+			xMaxRange = Math.max.apply(Math, indices);
+
+			//Define the true shape from offset = 0 index.
+			var shape = initialShape.map((y, i) => {
+				return y.map((point, ii) => {
+					point[1] = point[1] - xMinRange;
+					return point;
+				});
+			});
+
+			//Define the true range (which is simply max minus min range)
+			var xRange = xMaxRange - xMinRange;
+
+			//Push it to the shapes array.
+			shapes.push({
+				'character': alphabet[i],
+				'matrix': shape,
+				'xRange': xRange,
+				'yRange': shape.length
+			});
+
+		}
+
+		shapes.map((shape, i) => {
+			console.log(shape.character, shape.xRange, shape.yRange);
 		})
-
-		//Map the matrix's indices to a hashtable to get the min and max range of X
-		var xMinRange, xMaxRange;
-		var indices = [];
-
-		shapeOfA.map((y, i) => {
-			y.map((point, ii) => {
-				indices.push(point[1]);
-			});
-		});
-
-		xMinRange = Math.min.apply(Math, indices);
-		xMaxRange = Math.max.apply(Math, indices);
-
-		//Define the true shape from offset = 0 index.
-		var shape = shapeOfA.map((y, i) => {
-			return y.map((point, ii) => {
-				point[1] = point[1] - xMinRange;
-				return point;
-			});
-		});
-
-		//Define the true range (which is simply max minus min range)
-		var xRange = xMaxRange - xMinRange;
-
-		console.log(xRange, shape);
 
 	});
 
